2cfd2ad02c183c65c07ef9373168652602bc5b5e
Najathmhd <156327591+Najathmhd@users.noreply.github.com>
2024-11-01 17:51:03 +0530

    Add files via upload

---
+++ Applied-python/oop.py
@@ -0,0 +1,27 @@
+# class Warakapola:
+#     def __init__(self,name=None, place=None):
+#         self.name = name
+#         self.place = place
+    
+
+
+# name1 = Warakapola()
+# setattr(name1, 'name', 'najath')
+# print('1st nme is:', name1.name)
+
+# pls1 = Warakapola()
+# setattr(pls1,'place', 'masjid')
+# print('1st place is:', pls1.place)
+
+
+class Python:
+    def __init__(self, name=None):
+        self.name = name
+
+name1 = Python()
+setattr(name1, 'name', 'vithanage')
+print('1st teacher name is:', name1.name)
+
+name2 = Python()
+setattr(name2, 'name', 'Pubudu')
+print('2nd teacher name is:', name2.name)
